0486eefe8dcab50d17aee444846965b35d1fd40d
CrackerJaps <thecrackerjaps@gmail.com>
2022-05-09 00:09:16 -0700

    thru pt 208 - need to sort out sendgrid account before next vid

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,6 @@ const express = require('express')
 const userController = require('../controllers/userController')
 const authController = require('../controllers/authController')
 
-
-
 const router = express.Router()
 
 router.post('/signup', authController.signup)
@@ -19,7 +17,12 @@ router.use(authController.protect)
 router.patch('/updateMyPassword', authController.updatePassword)
 
 router.get('/me', userController.getMe, userController.getUser)
-router.patch('/updateMe', upload.single('photo'), userController.updateMe)
+router.patch(
+  '/updateMe',
+  userController.uploadUserPhoto,
+  userController.resizeUserPhoto,
+  userController.updateMe
+)
 router.delete('/deleteMe', userController.deleteMe)
 
 router.use(authController.restrictTo('admin'))
--- utils/email.js
+++ utils/email.js
@@ -1,34 +1,62 @@
 const nodemailer = require('nodemailer')
+const pug = require('pug')
+const { convert } = require('html-to-text')
 
-const sendEmail = async (options) => {
-  // 1) CREATE A TRANSPORTER
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  })
-
-  // 2) DEFINE EMAIL OPTIONS
-  const mailOptions = {
-    from: 'CrackerJaps <thecrackerjaps@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html:
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email
+    this.firstName = user.name.split(' ')[0]
+    this.url = url
+    this.from = `TheCrackerJaps <${process.env.EMAIL_FROM}>`
   }
 
-  // 3) SEND THE EMAIL
-  await transporter.sendMail(mailOptions)
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // SendGrid
+      return 1
+    }
+
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    })
+  }
+  // Send the actual email
+  async send(template, subject) {
+    // 1) RENDER HTML BASED ON A PUG TEMPLATE
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject
+    })
+    // 2) DEFINE EMAIL OPTIONS
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: convert(html, {
+        wordwrap: 130
+      })
+    }
+
+    // 3) CREATE A TRANSPORT AND SEND EMAIL
+
+    await this.newTransport().sendMail(mailOptions)
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours Family!')
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for 10 minutes)'
+    )
+  }
 }
-
-module.exports = sendEmail
-
-// service: 'Gmail ',
-// auth: {
-//   user: process.env.EMAIL_USERNAME,
-//   password: process.env.EMAIL_PASSWORD,
-//   },
-// Activate in gmail "less secure app" option
