d2297087ff73c6d06f2624827cd80ca93a9749c5
CrackerJaps <thecrackerjaps@gmail.com>
2022-05-19 12:40:10 -0700

    update email settings

--- utils/email.js
+++ utils/email.js
@@ -7,7 +7,7 @@ module.exports = class Email {
     this.to = user.email
     this.firstName = user.name.split(' ')[0]
     this.url = url
-    this.from = `TheCrackerJaps <${process.env.EMAIL_FROM}>`
+    this.from = `Natours <${process.env.EMAIL_FROM}>`
   }
 
   newTransport() {
