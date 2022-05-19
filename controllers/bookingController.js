0a8800f274ed03abcb725ee8dd36a30901b1715d
CrackerJaps <thecrackerjaps@gmail.com>
2022-05-18 20:15:40 -0700

    implemented stripe messages

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -11,7 +11,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 2) CREATE CHECKOUT SESSION
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
     // success_url: `${req.protocol}://${req.get('host')}/?tour=${
     //   req.params.tourId
     // }&user=${req.user.id}&price=${tour.price}`,
--- public/js/index.js
+++ public/js/index.js
@@ -5,6 +5,7 @@ import { login, logout } from './login'
 import { updateSettings } from './updateSettings'
 import { signup } from './signup'
 import { bookTour } from './stripe'
+import { showAlert } from './alerts'
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map')
@@ -92,3 +93,6 @@ if (bookBtn) {
     bookTour(tourId)
   })
 }
+
+const alertMessage = document.querySelector('body').dataset.alert
+if (alert) showAlert('success', alertMessage, 15)
